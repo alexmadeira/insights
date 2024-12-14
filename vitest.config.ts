de893dab782310f094be8d44f601ebdb2dfd3810
Alex Madeira <alex.c.madeira@gmail.com>
2024-12-14 17:26:21 -0300

    feat: configure `vitest`

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import path from 'node:path'
+
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    reporters: ['verbose'],
+    include: ['./src/__tests__/unit/**/*'],
+    includeTaskLocation: true,
+    coverage: {
+      reporter: 'html-spa',
+      include: ['src/**'],
+      exclude: ['**/@types/**', '**/__tests__/**'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@DOMTypes': path.resolve(__dirname, './src/domain/@types'),
+      '@CORTypes': path.resolve(__dirname, './src/core/@types'),
+      _DOM: path.resolve(__dirname, './src/domain'),
+      _DOMApp: path.resolve(__dirname, './src/domain/application'),
+      _DOMEnt: path.resolve(__dirname, './src/domain/enterprise'),
+      _COR: path.resolve(__dirname, './src/core'),
+      _TEST: path.resolve(__dirname, './src/__tests__'),
+    },
+  },
+})
