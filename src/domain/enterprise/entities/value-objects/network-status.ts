203db032548a3bb0a0c904f044ed4c6b4b7ebb8d
Alex Madeira <alex.c.madeira@gmail.com>
2024-12-16 19:58:01 -0300

    revert: remove post entity

--- src/domain/enterprise/entities/value-objects/network-status.ts
+++ src/domain/enterprise/entities/value-objects/network-status.ts
@@ -1,9 +1,8 @@
 import type { TENetworkStatus } from '@DOMTypes/enums/network'
 
+import { Status } from '_COR/entities/value-objects/status'
 import { networkStatusName } from '_DOM/constants/name/network'
 import { ZENetworkStatus } from '@DOMTypes/enums/network'
-
-import { Status } from './base/status'
 
 export class NetworkStatus extends Status<TENetworkStatus> {
   static create(value: string) {
--- src/domain/enterprise/entities/value-objects/reference-status.ts
+++ src/domain/enterprise/entities/value-objects/reference-status.ts
@@ -1,9 +1,8 @@
 import type { TEReferenceStatus } from '@DOMTypes/enums/reference'
 
+import { Status } from '_COR/entities/value-objects/status'
 import { referenceStatusName } from '_DOM/constants/name/reference'
 import { ZEReferenceStatus } from '@DOMTypes/enums/reference'
-
-import { Status } from './base/status'
 
 export class ReferenceStatus extends Status<TEReferenceStatus> {
   static create(value: string) {
