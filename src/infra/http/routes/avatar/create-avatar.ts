074749f3a3d012025f9a8ee44a7f6d801221bb53
Alex Madeira <alex.c.madeira@gmail.com>
2025-02-06 18:30:23 -0300

    fix: remove unused properties from `fastify build route`

--- src/infra/http/routes/avatar/create-avatar.ts
+++ src/infra/http/routes/avatar/create-avatar.ts
@@ -5,8 +5,7 @@ import { createAvatarController } from '_INFHttp/module/avatar'
 import { avatarCreateSchema } from '_INFHttp/schema/avatar-create'
 
 export const createAvatarRoute = (routeGroup: RouteGroup) =>
-  Route.get({
-    path: '/',
+  Route.post({
     routeGroup,
     summary: 'summary'.toUpperCase() + ': Create a new unlinked avatar',
     description: 'description'.toUpperCase() + ': Create a new unlinked avatar',
--- src/infra/http/schema/avatar-create.ts
+++ src/infra/http/schema/avatar-create.ts
@@ -2,6 +2,18 @@ import { RequestSchema } from '_INFCommon/request-schema'
 import z from 'zod'
 
 export const avatarCreateSchema = RequestSchema({
+  body: z
+    .object({
+      name: z.string().describe('Company ID'),
+      email: z.string().email().describe('Avatar ID'),
+    })
+    .describe('Query string for filtering or expanding data'),
+  params: z
+    .object({
+      companyId: z.string().optional().describe('Company ID'),
+      id: z.string().optional().describe('Avatar ID'),
+    })
+    .describe('Query string for filtering or expanding data'),
   querystring: z
     .object({
       include: z.string().optional().describe('Fields to include in the response'),
